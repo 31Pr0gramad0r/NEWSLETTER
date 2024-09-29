03e38056cf17f191aeaba2dc012e7bbe908ad006
31Pr0gramad0r <88546613+31Pr0gramad0r@users.noreply.github.com>
2024-09-29 20:22:42 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const sidebar = document.getElementById('sidebar');
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const tabs = document.querySelectorAll('nav ul li a');
-        const contents = document.querySelectorAll('.tab-content');
-
-        tabs.forEach(tab => {
-            tab.addEventListener('click', function (event) {
-                event.preventDefault();
-                const target = tab.getAttribute('data-tab');
-
-                contents.forEach(content => {
-                    content.classList.add('hidden');
-                    if (content.id === target) {
-                        content.classList.remove('hidden');
-                        content.classList.add('active');
-                    } else {
-                        content.classList.remove('active');
-                    }
-                });
-            });
-        });
+    menuToggle.addEventListener('click', function () {
+        sidebar.classList.toggle('active');
     });
-    
+});
