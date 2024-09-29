dd098919a045e75b743f71684f869bf9d56628eb
31Pr0gramad0r <88546613+31Pr0gramad0r@users.noreply.github.com>
2024-09-29 22:50:52 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('section');
 
-    // Función para aplicar el efecto de desvanecimiento al párrafo completo
-    function showParagraphWithFade(element) {
-        element.classList.add('fade-in');  // Agregamos la clase de desvanecimiento
-    }
-
-    // Iniciar el efecto de desvanecimiento en los párrafos cuando están en la vista
-    function handleScroll() {
-        sections.forEach(section => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-                const typewriteElement = section.querySelector('.typewrite');
-                if (typewriteElement && !typewriteElement.classList.contains('typed')) {
-                    showParagraphWithFade(typewriteElement);  // Aplicamos el desvanecimiento al párrafo
-                    typewriteElement.classList.add('typed');  // Evitar que se repita el efecto
-                }
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
             }
         });
-    }
+    }, { threshold: 0.1 });
 
-    // Evento para manejar el scroll
-    window.addEventListener('scroll', handleScroll);
+    sections.forEach(section => {
+        observer.observe(section);
+    });
 });
